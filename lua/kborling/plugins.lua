88907641e7e8ab0a6564e40393f7b97f28fc7ba9
Kevin Borling <kborling@protonmail.com>
2022-11-17 21:47:28 -0500

    Add Org Mode Setup

--- lua/kborling/plugins.lua
+++ lua/kborling/plugins.lua
@@ -46,4 +46,29 @@ packer.startup(function(use)
   use 'timuntersberger/neogit' -- Magit clone
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
+
+  use {'nvim-orgmode/orgmode', config = function()
+    require('orgmode').setup{}
+  end
+  }
+  -- Load custom treesitter grammar for org filetype
+  require('orgmode').setup_ts_grammar()
+
+  -- Treesitter configuration
+  require('nvim-treesitter.configs').setup {
+    -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+    -- highlighting will fallback to default Vim syntax highlighting
+    highlight = {
+      enable = true,
+      -- Required for spellcheck, some LaTex highlights and
+      -- code block highlights that do not have ts grammar
+      additional_vim_regex_highlighting = {'org'},
+    },
+    ensure_installed = {'org'}, -- Or run :TSUpdate org
+  }
+
+  require('orgmode').setup({
+    org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+    org_default_notes_file = '~/Dropbox/org/refile.org',
+  })
 end)
