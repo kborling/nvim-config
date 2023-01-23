e17a1de648531705eb0cd53a80c441393c35a61a
Kevin Borling <kborling@protonmail.com>
2023-01-22 19:43:41 -0500

    Update lsp colors

--- after/plugin/lsp-colors.rc.lua
+++ after/plugin/lsp-colors.rc.lua
@@ -2,8 +2,8 @@ local status, colors = pcall(require, "lsp-colors")
 if (not status) then return end
 
 colors.setup {
-  Error = "#db4b4b",
-  Warning = "#e0af68",
-  Information = "#0db9d7",
-  Hint = "#10B981"
+  Error = "#ff0038",
+  Warning = "#ff7700",
+  Information = "#47eae0",
+  Hint = "#00ff77"
 }
