a13e00a97736faa8f7b312e08eb96c13ac442f92
Kevin Borling <kborling@protonmail.com>
2023-02-12 19:52:31 -0500

    Update Lsp Config
    Remove unused settings

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -9,11 +9,11 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 local enable_format_on_save = function(_, bufnr)
   vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
   vim.api.nvim_create_autocmd("BufWritePre", {
-    group = augroup_format,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ bufnr = bufnr })
-    end,
+      group = augroup_format,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
   })
 end
 
@@ -37,146 +37,131 @@ local on_attach = function(client, bufnr)
 end
 
 protocol.CompletionItemKind = {
-  '', -- Text
-  '', -- Method
-  '', -- Function
-  '', -- Constructor
-  '', -- Field
-  '', -- Variable
-  '', -- Class
-  'ﰮ', -- Interface
-  '', -- Module
-  '', -- Property
-  '', -- Unit
-  '', -- Value
-  '', -- Enum
-  '', -- Keyword
-  '﬌', -- Snippet
-  '', -- Color
-  '', -- File
-  '', -- Reference
-  '', -- Folder
-  '', -- EnumMember
-  '', -- Constant
-  '', -- Struct
-  '', -- Event
-  'ﬦ', -- Operator
-  '', -- TypeParameter
+    '', -- Text
+    '', -- Method
+    '', -- Function
+    '', -- Constructor
+    '', -- Field
+    '', -- Variable
+    '', -- Class
+    'ﰮ', -- Interface
+    '', -- Module
+    '', -- Property
+    '', -- Unit
+    '', -- Value
+    '', -- Enum
+    '', -- Keyword
+    '﬌', -- Snippet
+    '', -- Color
+    '', -- File
+    '', -- Reference
+    '', -- Folder
+    '', -- EnumMember
+    '', -- Constant
+    '', -- Struct
+    '', -- Event
+    'ﬦ', -- Operator
+    '', -- TypeParameter
 }
 
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" },
-  capabilities = capabilities
-}
-
-nvim_lsp.eslint.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" },
+    capabilities = capabilities
 }
 
 nvim_lsp.angularls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-
-nvim_lsp.cssls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.tailwindcss.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.html.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.emmet_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.svelte.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.omnisharp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.jsonls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-
-nvim_lsp.astro.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.gopls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.clangd.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 }
 
 nvim_lsp.rls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "rustup", "run", "stable", "rust-analyzer" },
-  settings = {
-    rust = {
-      unstable_features = true,
-      build_on_save = false,
-      all_features = true,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "rustup", "run", "stable", "rust-analyzer" },
+    settings = {
+        rust = {
+            unstable_features = true,
+            build_on_save = false,
+            all_features = true,
+        },
     },
-  },
 }
 
-nvim_lsp.sumneko_lua.setup {
-  capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-    enable_format_on_save(client, bufnr)
-  end,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
+nvim_lsp.lua_lsp.setup {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      enable_format_on_save(client, bufnr)
+    end,
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
 
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false
+            },
+        },
     },
-  },
 }
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-  underline = true,
-  update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
-  severity_sort = true,
-}
-)
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = { spacing = 4, prefix = "●" },
+        severity_sort = true,
+    }
+    )
 
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
@@ -186,11 +171,11 @@ for type, icon in pairs(signs) do
 end
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●'
-  },
-  update_in_insert = true,
-  float = {
-    source = "always", -- Or "if_many"
-  },
+    virtual_text = {
+        prefix = '●'
+    },
+    update_in_insert = true,
+    float = {
+        source = "always", -- Or "if_many"
+    },
 })
