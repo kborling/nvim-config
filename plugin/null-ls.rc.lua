a2212bf6ff16d099d388d1e1732ee3b5a1080d9e
Kevin Borling <kborling@protonmail.com>
2023-02-16 20:31:38 -0500

    Fix null-ls commands for Windows

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -5,46 +5,61 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local lsp_formatting = function(bufnr)
   vim.lsp.buf.format({
-    filter = function(client)
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
+      filter = function(client)
+        return client.name == "null-ls"
+      end,
+      bufnr = bufnr,
   })
 end
 
-null_ls.setup {
-  sources = {
-    null_ls.builtins.formatting.stylelint,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.stylelint,
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
-    -- Eslint format on save
-    null_ls.builtins.formatting.eslint_d.with({
-      formatting_format = '[eslint] --fix-to-stdout --stdin --stdin-filename %f:%l:%c: %m'
-    }),
-    -- Typescript extras
-    require("typescript.extensions.null-ls.code-actions"),
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          lsp_formatting(bufnr)
-        end,
-      })
-    end
+-- Add .cmd to lsp commands on windows so they can be found
+local function add_cmd_suffix(cmd)
+  if vim.fn.has('win32') == 1 then
+    return cmd .. '.cmd'
   end
+  return cmd
+end
+
+null_ls.setup {
+    sources = {
+        null_ls.builtins.formatting.stylelint.with({
+            command = add_cmd_suffix('stylelint')
+        }),
+        null_ls.builtins.diagnostics.stylelint.with({
+            command = add_cmd_suffix('stylelint')
+        }),
+        null_ls.builtins.diagnostics.eslint_d.with({
+            diagnostics_format = '[eslint] #{m}\n(#{c})',
+            command = add_cmd_suffix('eslint_d')
+        }),
+        null_ls.builtins.formatting.eslint_d.with({
+            formatting_format = '[eslint] --fix-to-stdout --stdin --stdin-filename %f:%l:%c: %m',
+            command = add_cmd_suffix('eslint_d')
+        }),
+        null_ls.builtins.code_actions.eslint_d.with({
+            command = add_cmd_suffix('eslint_d')
+        }),
+        -- Typescript extras
+        require("typescript.extensions.null-ls.code-actions"),
+    },
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              lsp_formatting(bufnr)
+            end,
+        })
+      end
+    end
 }
 
 vim.api.nvim_create_user_command(
-  'DisableLspFormatting',
-  function()
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-  end,
-  { nargs = 0 }
+    'DisableLspFormatting',
+    function()
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+    end,
+    { nargs = 0 }
 )
