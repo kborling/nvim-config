8636972f8afc5279117d75421045321404a9567b
Kevin Borling <kborling@protonmail.com>
2024-11-20 19:09:12 -0500

    use powershell instead of nu for windows

--- lua/kborling/windows.lua
+++ lua/kborling/windows.lua
@@ -8,15 +8,15 @@ vim.opt.tabstop      = 4
 vim.opt.linespace    = 1
 
 -- Nushell
-vim.opt.shell        = 'nu.exe'
-vim.opt.shellcmdflag = '-c'
-vim.opt.shellxquote  = ''
-vim.opt.shellquote   = ''
-
--- Powershell
--- vim.opt.shell        = 'powershell.exe'
--- vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+-- vim.opt.shell        = 'nu.exe'
+-- vim.opt.shellcmdflag = '-c'
 -- vim.opt.shellxquote  = ''
 -- vim.opt.shellquote   = ''
--- vim.opt.shellpipe    = '| Out-File -Encoding UTF8 %s'
--- vim.opt.shellredir   = '| Out-File -Encoding UTF8 %s'
+
+-- Powershell
+vim.opt.shell        = 'powershell.exe'
+vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+vim.opt.shellxquote  = ''
+vim.opt.shellquote   = ''
+vim.opt.shellpipe    = '| Out-File -Encoding UTF8 %s'
+vim.opt.shellredir   = '| Out-File -Encoding UTF8 %s'
